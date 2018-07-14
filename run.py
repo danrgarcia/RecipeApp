1782c6358d987e2cd7696bef3e7960755befc3b4
Daniel Garcia <daniel.garcia2@gmail.com>
2018-07-14 12:03:35 -0700

    Initial version of Daniels Recipe's App

---
+++ run.py
@@ -0,0 +1,10 @@
+# run.py
+
+# Add yoru settings here... this is a temporary location, as the settings for a Flask app
+# should be stored seperate from your main program
+DEBUG = True
+
+from project import app
+
+if __name__ == "__main__":
+    app.run()
